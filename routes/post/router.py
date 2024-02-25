cdd5ca19f00b2339d40ed7334257eadf9239a6fe
Yann Sko <yskonieczny@gmail.com>
2024-02-25 20:59:20 +0100

    test_passage en list de dico
    {
        "detail": [
            {
                "type": "list_type",
                "loc": [
                    "body"
                ],
                "msg": "Input should be a valid list",
                "input": {
                    "games_data": [
                        {
                            "_id": "65d47a9bc3020b2ae2f93980",
                            "Title": "Game 1",
                            "Release Date": "2022-01-01",
                            "Developer": "Developer 1",
                            "Publisher": "Publisher 1",
                            "Genres": [
                                "Action",
                                "Adventure"
                            ],
                            "User Score": 8.0,
                            "User Ratings Count": 100
                        },
                        {
                            "_id": "65d47a9bc3020b2ae2f93981",
                            "Title": "Game 2",
                            "Release Date": "2022-02-01",
                            "Developer": "Developer 2",
                            "Publisher": "Publisher 2",
                            "Genres": [
                                "RPG"
                            ],
                            "User Score": 7.5,
                            "User Ratings Count": 80
                        }
                    ]
                },
                "url": "https://errors.pydantic.dev/2.6/v/list_type"
            }
        ]
    }

--- routes/post/router.py
+++ routes/post/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Request, HTTPException, status
 from bson.objectid import ObjectId
+from typing import List
 
 postrouter = APIRouter()
 
@@ -20,27 +21,24 @@ async def add_game(request: Request, game_data: dict):
     
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="echec critique")
 
-@postrouter.post("/games/multiple", response_description="aj dé games", status_code=status.HTTP_201_CREATED)
-async def add_x_games(request: Request, games_data: list):
-    """ games games"""
-    # list verif
+@postrouter.post("/games/multiple", response_description="Add multiple games", status_code=status.HTTP_201_CREATED)
+async def add_x_games(request: Request, games_data: List[dict]):  
+    """ Add multiple games """
+   
     inserted_games_ids = []
     
-    # on  parcourt list de jeu
-    for game_data in games_data:
-        
-        inserted_game = request.app.database.games.insert_one(game_data)
-        
-        
-        if inserted_game.inserted_id:
-           
-            inserted_games_ids.append(str(inserted_game.inserted_id))
-        else:
-            # echec
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"echec sur: {game_data}")
     
-    # renvoie les ids des jeux crése
+    inserted_result = request.app.database.games.insert_many(games_data)
+    
+    
+    if inserted_result.inserted_ids:
+        inserted_games_ids.extend([str(game_id) for game_id in inserted_result.inserted_ids])
+    else:
+        
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Failed to insert games")
+    
+    
     return {
-        "message": "ça va game fort",
+        "message": "Games added successfully",
         "inserted_games_ids": inserted_games_ids
     }
--- routes/put/router.py
+++ routes/put/router.py
@@ -18,3 +18,28 @@ async def update_game(game_id: str, request: Request):
         return updated_game
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No game with that id")
 
+@putrouter.put("/games/multiple", response_description="Update multiple games")
+async def update_multiple_games(request: Request):
+    """ Update multiple games """
+    updated_games_data = await request.json()
+    updated_games_ids = []
+    for updated_game_data in updated_games_data:
+        game_id = updated_game_data.get('_id')
+        if game_id:
+            game_id = game_id.strip()
+            updated_game = request.app.database.games.find_one_and_update(
+                {"_id": ObjectId(game_id)},
+                {"$set": updated_game_data},
+                return_document=True
+            )
+            if updated_game:
+                updated_game['_id'] = str(updated_game['_id'])
+                updated_games_ids.append(str(game_id))
+            else:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No game with id {game_id}")
+        else:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="'_id' field is required for each game")
+    return {
+        "message": "Games updated successfully",
+        "updated_games_ids": updated_games_ids
+    }
